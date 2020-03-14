9ee8f7c086ab52023704c2110192b3765f4a26cc
niks <maniyanikul99999@gmail.com>
2020-03-14 17:43:51 +0530

    Set function Add and Display Data

---
+++ CoreDataDemo/DisplayDataVC.swift
@@ -0,0 +1,69 @@
+//
+//  DisplayDataVC.swift
+//  CoreDataDemo
+//
+//  Created by nikunj on 14/03/20.
+//  Copyright © 2020 Nikul. All rights reserved.
+//
+struct UserModel  {
+    var firstName : String!
+    var lastName : String!
+    init(_ fname : String,_ lname : String) {
+        firstName = fname
+        lastName = lname
+    }
+}
+
+import UIKit
+import CoreData
+
+class DisplayDataVC: UIViewController {
+
+    var fetchRecord  = [UserModel]()
+    
+    @IBOutlet var tableView : UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.delegate = self
+        fetchData()
+    }
+    
+    
+    // Fetch Data From User Entity
+    func fetchData()
+    {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedObject = appDelegate.persistentContainer.viewContext
+        let fetchReq = NSFetchRequest<NSFetchRequestResult>.init(entityName: "User")
+        
+        do{
+            let data = try! managedObject.fetch(fetchReq)
+            for obj in (data as? [NSManagedObject])!
+            {
+                let firstName = obj.value(forKey: "firstname") as! String
+                let lastName = obj.value(forKey: "firstname") as! String
+                
+                fetchRecord.append(UserModel.init(firstName , lastName))
+            }
+        }
+        tableView.reloadData()
+        
+    }
+    
+}
+
+// Table Delegate
+extension DisplayDataVC : UITableViewDelegate,UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fetchRecord.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UserCell", for: indexPath) as! UserCell
+        cell.dataArray = fetchRecord[indexPath.row]
+        return cell
+    }
+}
--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
-
+import CoreData
 class ViewController: UIViewController {
 
+    @IBOutlet var firstNameField : UITextField!
+    @IBOutlet var lastnameField : UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Test")
+    }
+    
+    @IBAction func onClickSaveUserData()
+    {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "User", in: managedContext)
+        
+        let user =  NSManagedObject.init(entity: entity!, insertInto: managedContext)
+        user.setValue(firstNameField.text, forKey: "firstname")
+        user.setValue(lastnameField.text, forKey: "lastname")
+        
+        do{
+            try! managedContext.save()
+            print("Saved successfully")
+        }
+        catch let err {
+            print(err)
+        }
+        
+    }
+    
+    @IBAction func onClickShowUserData()
+    {
+        self.performSegue(withIdentifier: "DisplayDataVC", sender: nil)
     }
 
 
