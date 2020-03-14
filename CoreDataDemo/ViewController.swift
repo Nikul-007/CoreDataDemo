212765b2d31376475c6a342bbc95c067d37404d9
niks <maniyanikul99999@gmail.com>
2020-03-14 12:09:40 +0530

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by nikunj on 14/03/20.
+//  Copyright © 2020 Nikul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
